ffc07099f407cbb3c14107027d856171a43d734d
AdvaithM26 <65194982+AdvaithM26@users.noreply.github.com>
2020-06-29 18:57:31 -0400

    moved files (again)

---
+++ main/cookie.js
@@ -0,0 +1,26 @@
+function setCookie(cookieName, cookieValue, daysUntilExpiration) {
+	var date = new Date();
+	date.setTime(date.getTime() + daysUntilExpiration * 86400000);
+	var expires = "expires=" + date.toGMTString();
+	document.cookie = cookieName + "=" + cookieValue + ";" + expires + ";path=/";
+}
+
+function getCookie(cookieName) {
+	var name = cookieName + "=";
+	var decodedCookie = decodeURIComponent(document.cookie);
+	var ca = decodedCookie.split(";");
+	for (var i = 0; i < ca.length; i++) {
+		var c = ca[i];
+		while (c.charAt(0) == " ") {
+			c = c.substring(1);
+		}
+		if (c.indexOf(name) == 0) {
+			return c.substring(name.length, c.length);
+		}
+	}
+	return "";
+}
+
+function deleteCookie(cookieName) {
+   document.cookie = cookieName + '=; expires=Thu, 01 Jan 1970 00:00:01 GMT; path=/'
+ }
